2241966b48ef4efc88632b68b6bbe54f84ef2972
Georgi Dimitrov <gogo3888@mail.bg>
2021-07-09 09:14:33 +0300

    Add files via upload

---
+++ jj.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+if (( "$#" < 1 )); then echo "use -h for help."; fi
+
+option=$1
+
+
+case $option in
+
+ -eu)
+ echo "Login to EU"
+ tsh login --proxy=eu-tlp.sbtech.info:443;;
+ -us)
+ echo "Logim to US"
+ tsh login --proxy=us-tlp.sbtech.info:443;;
+ -cl)
+ tsh clusters ;;
+ -kls)
+ echo "K8s clusters"
+ tsh kube ls ;;
+ -kl)
+ echo "Login to k8s cluwter"
+ tsh kube login $2 ;;
+ -ns)
+ echo "Node search"
+ tsh ls |awk '{print $1}'|grep $2;;
+  -nl)
+ echo "List available nodes"
+ tsh ls |awk '{print $1}';;
+ -l)
+ echo "Login via ssh."
+ node=$2
+ tsh ssh root@$node ;;
+ -lo)
+ tsh logout ;;
+ -h)
+ echo "Use -eu to EU or -us for US"
+ echo "Use -kls to list k8s clusters"
+ echo "Use -kl to login to k8s cluster"
+ echo "Use -cl to list clusters, -nl to list nodes, use -login to login via ssh."
+ echo "Use -ns to search for node"
+ echo "Use -l to loggo via ssh to neode"
+ echo "Use -lo to logout from teleport destination."
+ 
+ 
+esac
+
+
+
+
+
+
+
